61d08f24ce20d329f514edf705b4b16f16e635f3
Mahedihasanimam <mdmehedihasen678@gmail.com>
2024-09-10 09:36:27 +0600

    deploy project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./App.{js,jsx,ts,tsx}', './screens/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        customColor: '#1C1C1C',
+      },
+    },
+  },
+  plugins: [],
+};
